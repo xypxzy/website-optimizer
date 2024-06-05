ad9540123e362de857115ad869bae3ebbc956caf
Dastan <xypxzy@gmail.com>
2024-06-05 21:18:46 +0600

    add test for jwt and password util func

---
+++ service/auth/password_test.go
@@ -0,0 +1,33 @@
+package auth
+
+import "testing"
+
+func TestHashPassword(t *testing.T) {
+	hash, err := HashPassword("password")
+	if err != nil {
+		t.Errorf("error hashing password: %v", err)
+	}
+
+	if hash == "" {
+		t.Error("expected hash to be not empty")
+	}
+
+	if hash == "password" {
+		t.Error("expected hash to be different from password")
+	}
+}
+
+func TestComparePassword(t *testing.T) {
+	hash, err := HashPassword("password")
+	if err != nil {
+		t.Errorf("error hashing password: %v", err)
+	}
+
+	if !ComparePassword(hash, []byte("password")) {
+		t.Error("expected password to match hash")
+	}
+
+	if ComparePassword(hash, []byte("notpassword")) {
+		t.Error("expected password to not match hash")
+	}
+}
