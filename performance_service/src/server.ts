91a9b81911212cd80531b5aac5575ed483c5279d
dastan <chynybekuuludastan@gmail.com>
2025-02-08 21:20:52 +0600

    feat(performance_service): refactor gRPC service and add performance data proto definitions

--- performance_service/src/server.ts
+++ performance_service/src/server.ts
@@ -1,8 +1,9 @@
 import * as grpc from '@grpc/grpc-js'
 import * as protoLoader from '@grpc/proto-loader'
+import path from 'path'
 import { PerformanceAnalyzer } from './services/performanceAnalyzer'
 
-const PROTO_PATH = __dirname + '/protos/performance_service.proto'
+const PROTO_PATH = path.resolve(__dirname, './protos/performance_service.proto')
 const packageDefinition = protoLoader.loadSync(PROTO_PATH, {
 	keepCase: true,
 	longs: String,
@@ -14,7 +15,7 @@ const proto = grpc.loadPackageDefinition(packageDefinition) as any
 
 const server = new grpc.Server()
 
-server.addService(proto.PerformanceService.service, {
+server.addService(proto.performance.PerformanceAnalyzer.service, {
 	Analyze: async (
 		call: grpc.ServerUnaryCall<any, any>,
 		callback: grpc.sendUnaryData<any>
@@ -29,11 +30,10 @@ server.addService(proto.PerformanceService.service, {
 
 const PORT = process.env.PORT || 50053
 server.bindAsync(
-	`0.0.0.0.${PORT}`,
+	'0.0.0.0:50051',
 	grpc.ServerCredentials.createInsecure(),
 	(err, port) => {
 		if (err) throw err
 		console.log(`🚀 Performance analyzer listening on port ${port}`)
-		server.start()
 	}
 )
